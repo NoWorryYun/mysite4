88541bf70d5aae19a967f6dd122172d1678284ba
윤무걱정 <dbstjd3201@naver.com>
2022-06-21 15:14:03 +0900

    board

---
+++ src/main/java/com/javaex/service/BoardService.java
@@ -0,0 +1,31 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.BoardDao;
+import com.javaex.vo.BoardVo;
+
+@Service
+public class BoardService {
+
+	//필드
+	@Autowired
+	private BoardDao boardDao;
+	//생성자
+	
+	//GS
+	
+	//일반
+	public List<BoardVo> boardList(){
+		
+		return boardDao.boardList();
+	}
+	
+	public BoardVo readBoard(int no) {
+		
+		return boardDao.readBoard(no);
+	}
+}
